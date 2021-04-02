74663e855fe682347af5c0fb7a38f7005c829f18
hiwad rashad <hiwadrashad112@hotmail.com>
2021-04-02 16:01:59 +0200

    Add project files.

---
+++ PageFrame/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PageFrame.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
